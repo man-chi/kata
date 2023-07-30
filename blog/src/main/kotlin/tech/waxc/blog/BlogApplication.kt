dd417067a8d5789c50b2ed8603c3a1e2f75e281d
man-chi <manchi@waxc.tech>
2023-07-30 17:37:20 +0800

    regenerated antora

---
+++ blog/src/main/kotlin/tech/waxc/blog/BlogApplication.kt
@@ -0,0 +1,11 @@
+package tech.waxc.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogApplication>(*args)
+}
